08f86de380e78b9ffe5f4a3563f11a2d4bcb96c8
jbok4 <jbok4@aol.com>
2017-04-08 18:45:28 -0400

    seed file for doctors info loaded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+doctors = Doctor.create([{ name: 'Zach Silverzweig', image: "https://cipherhealth.com/wp-content/uploads/2016/08/L_ZachSilverzweig-min.jpg" }, 
+  { name: 'Alex Liu', image: "https://cipherhealth.com/wp-content/uploads/2016/08/AlexLiu-min.jpg" },
+  { name: 'Alisha McWilliams', image: "https://cipherhealth.com/wp-content/uploads/2016/08/alishamcwilliams-min.jpg" },
+  { name: 'Nick Cherry', image: "https://cipherhealth.com/wp-content/uploads/2016/08/NT9A7225-min.jpg" }
+  ])
